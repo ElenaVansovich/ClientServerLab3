a6ab3f89dfabdf83ef44b2f70f2195f23c27c3e2
ElenaVansovich <elena.vansovich.96@mail.ru>
2016-12-05 17:21:16 +0300

    Add transfering  arguments of command line from client to server

--- fsclient.c
+++ fsclient.c
@@ -4,32 +4,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 #define BUF_SIZE 2048
  
-int main ()
-{
+int main (int argc, char* argv[]) {
 	int sock, connection, bytes = 0;      
 	struct sockaddr_in sock_addr;
 
 	char cmd[BUF_SIZE], buf[BUF_SIZE];
 
+	char addr[BUF_SIZE];
+
     	sock = socket(AF_INET, SOCK_STREAM, 0);
 
-	if(sock < 0)
-	{
+	if (sock < 0) {
 		perror("socket error\n");
 		exit(1);
 	}
+	
+	if (argc > 0) {
+		strcpy(addr, argv[1]); 
+	}
+	else {
+		perror("arguments error\n");
+	}
 
-	sock_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	sock_addr.sin_addr.s_addr = inet_addr(addr);
     	sock_addr.sin_family = AF_INET;
     	sock_addr.sin_port = htons(3425);
     
 	connection = connect(sock, (struct sockaddr *)&sock_addr, sizeof(sock_addr));
 
-   	if(connection < 0)
-	{
+   	if (connection < 0) {
 		perror("connect error\n");
 		exit(2);
 	}
@@ -40,8 +47,7 @@ int main ()
 	send(sock, cmd, BUF_SIZE, 0);
 
 	bytes = recv(sock, buf, BUF_SIZE, 0);
-	if(bytes < 0)
-	{
+	if (bytes < 0) {
 		perror("recv error\n");
 		exit(2);
 	}
--- fsserver.c
+++ fsserver.c
@@ -10,8 +10,7 @@
 
 #define BUF_SIZE 2048
 
-void cmd(void* param)
-{
+void cmd(void* param) {
 	int sock = (intptr_t)param;
 	char buf[BUF_SIZE];
 	int bytes;
@@ -19,23 +18,20 @@ void cmd(void* param)
 	FILE * f;
 
 	bytes = recv(sock, buf, BUF_SIZE, 0);
-	if(bytes <= 0)
-	{
+	if (bytes <= 0) {
 		perror("recv error\n");
 		return;
 	}
 
-	printf("\nReceive command: %s", buf);
+	printf("\nReceive text: %s", buf);
 
 	f = popen(buf, "r");
-	if (f == NULL)
-	{
+	if (f == NULL) {
 		perror("popen error\n");
 		return;
 	}
 	int len = -1;
-	while(len != 0)
-	{
+	while (len != 0) {
 		len = fread(buf, 1, BUF_SIZE, f);
 	}
 
@@ -46,56 +42,48 @@ void cmd(void* param)
 	send(sock, buf, bytes, 0);
 }
 
-int main()
-{
+int main() {
 	int sock, listener;
 	struct sockaddr_in sock_addr; 
 	
 	listener = socket(AF_INET, SOCK_STREAM, 0);
-	if(listener < 0)
-	{
+	if (listener < 0) {
 		perror("socket error");
 		exit(1);
 	}
 
 	sock_addr.sin_family = AF_INET;
 	sock_addr.sin_port = htons(3425);
-	sock_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	sock_addr.sin_addr.s_addr = inet_addr("0.0.0.0");
 
-	if(bind(listener, (struct sockaddr *)&sock_addr, sizeof(sock_addr)) < 0)
-	{
+	if(bind(listener, (struct sockaddr *)&sock_addr, sizeof(sock_addr)) < 0) {
 		perror("bind error");
 		exit(2);
 	}
 
 	listen(listener, 1);
-	while(1)
-	{
+	while(1) {
 		sock = accept(listener, NULL, NULL);
 
-		if(sock < 0)
-		{
+		if(sock < 0) {
 			perror("accept error");
 			return 1;
 		}
 		#ifdef PROCESS
 			pid_t id = 0;
 			id = fork();
-			if (id == 0)
-			{
+			if (id == 0) {
 				cmd((void *)(intptr_t)sock);
 				return 0;
 			}
-			else
-			{
+			else {
 				perror("fork error");
 				exit(1);
 			}
 		#else
 			pthread_t thread1;
 			int result = pthread_create(&thread1, NULL, (void *)cmd, (void *)(intptr_t)sock);
-			if (result != 0)
-			{
+			if (result != 0) {
 				perror("pthread_create error");
 			}
 		#endif
